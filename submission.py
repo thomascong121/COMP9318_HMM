506d1eee916295ea584d5c43145ba9faef083a0d
10Cent01 <txobelisk@hotmail.com>
2019-04-24 17:43:06 +1000

    started question 2

--- submission.py
+++ submission.py
@@ -25,7 +25,7 @@ def file_parser(fname):
         df = pd.DataFrame(dfl, columns = ['state1', 'state2','frequency']) 
         df= df.apply(pd.to_numeric, errors="ignore")
     return nstates,states,df
-nstates,all_states,df = file_parser("./dev_set/State_File")
+# nstates,all_states,df = file_parser("./dev_set/State_File")
 
 #create transition matrix
 def transition_matrix(nstates,all_states,df):
@@ -50,11 +50,11 @@ def transition_matrix(nstates,all_states,df):
             matrx[i][j]=(n_ij+1)/(n_i+nstates-1)
     return matrx
 #========================Emission prob=======================               
-nsymbols,all_symbols,df1 = file_parser("./dev_set/Symbol_File")
+# nsymbols,all_symbols,df1 = file_parser("./dev_set/Symbol_File")
 def emission_prob(nsymbols,df1,state,symbol):
     #if state1 value in the dataframe
-    if(state in df.state1.values):
-        df_t = df.loc[df.state1==state]
+    if(state in df1.state1.values):
+        df_t = df1.loc[df1.state1==state]
         n_i = df_t.frequency.sum()
         #if state2 value in the dataframe
         if(symbol in df_t.state2.values):
@@ -67,7 +67,7 @@ def emission_prob(nsymbols,df1,state,symbol):
     return emission_prob     
 #=======================tokenize==============================
 def tokenize(address):
-    aprime = re.split('\s+',a)
+    aprime = re.split('\s+', address)
     for i in aprime:
         i = i.strip()
     result = []
@@ -83,10 +83,61 @@ def viterbi_algorithm(State_File, Symbol_File, Query_File): # do not change the 
     nsymbols,all_symbols,df1 = file_parser(Symbol_File)
     tran_matrx = transition_matrix(nstates,all_states,df)
 
+    with open(Query_File) as f:
+        for l in f:
+            tmp = tokenize(l)
+            print(tmp)
 
 # Question 2
 def top_k_viterbi(State_File, Symbol_File, Query_File, k): # do not change the heading of the function
-    pass # Replace this line with your implementation...
+    nstates,all_states,df = file_parser(State_File)
+    nsymbols,all_symbols,df1 = file_parser(Symbol_File)
+    tran_matrx = transition_matrix(nstates,all_states,df)
+    result_mat = []
+
+    with open(Query_File) as f:
+        # loop through each query
+        for l in f:
+            line = tokenize(l)
+            col1 = []
+            col2 = []
+            # store first column
+            for i in range(nstates):
+                col1.append(emission_prob(nsymbols, df1, i, 0) /25)
+            print(col1)
+            result_mat.append(col1)
+
+            # loop through each char/element of current query.
+            for ele in line[1:]:
+                # as for each state
+                for j in df1.state1.values:
+                    # col2.append(max([tran_matrx[i][j] * col1[i] * emission_prob(nsymbols, df1, i, ele) for i in range(nstates)]))
+                    tmp = []
+                    for i in range(nstates):
+                        emit = emission_prob(nsymbols, df1, i, ele)
+                        tmp.append()
+
+                    col2.append(max([tran_matrx[i][j] * col1[i] *  ]))
+
+                result_mat.append(col2)
+                col1 = col2 # assign new col to be the previous col
+
+            # print(result_mat)
+            # find the sequence of states
+            for cols in result_mat:
+                biggest = max(cols)
+                for i,e in enumerate(cols):
+                    if e==biggest:
+                        ind = i
+                        break
+                print(ind+1 ' - ', end='')
+            print('.')
+            result_mat = []
+
+    return
+top_k_viterbi('toy_example/State_File', 'toy_example/Symbol_File', 'toy_example/Query_File', 3)
+# top_k_viterbi('dev_set/State_File', 'dev_set/Symbol_File', 'dev_set/Query_File', 3)
+
 
 
 # Question 3 + Bonus
